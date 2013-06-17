a51422ff22ea93a6948fcb065a4520568a0a5085
Byung Hoon Cho <red1004g@gmail.com>
2013-06-17 20:10:25 +0900

    Initial Commit

---
+++ LogTracer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LogTracer
+//
+//  Created by Cho Byung Hoon on 13. 6. 17..
+//  Copyright (c) 2013년 Cho Byung Hoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LogTracer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LogTracer
+//
+//  Created by Cho Byung Hoon on 13. 6. 17..
+//  Copyright (c) 2013년 Cho Byung Hoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
